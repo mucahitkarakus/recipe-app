f6d8d455918d29627aabed150399fd73693403ba
mucahitkarakus <mocococo.80@gmail.com>
2022-10-25 17:20:39 +0300

    login done created home page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,24 +10,29 @@ const Login = () => {
           Login to See All of the Delicious Recipes
         </p>
       </div>
-      <div className="m-auto mt-20 items-center justify-center w-[400px] h-[400px] bg-blue-200 rounded-xl">
+      <div className="flex flex-col m-auto mt-20 items-center justify-center w-[400px] h-[400px] bg-blue-200 rounded-xl">
         <div>
           <img className="w-[150px] h-[150px]  m-auto " src={resim} alt="" />
         </div>
-        <div className="block mx-auto my-[1rem] p-[.4rem] border-none outline-none font-[1.5rem] border-b  ">
-          <input type="text" name="name" placeholder="Your name" className="" />
+        <div className="mx-auto my-[1rem] p-[.4rem] border-none outline-none font-[1.5rem] border-b  ">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            className="h-[2rem] w-[20rem] text-center outline-none border-b rounded-xl font-[2rem]   "
+          />
         </div>
         <div>
           <input
             type="password"
             name="pass"
             placeholder="Your password"
-            className=""
+            className="h-[2rem] w-[20rem] text-center outline-none  rounded-xl font-[2rem] border-b-2 border-red-700   "
           />
         </div>
         <button
           type="submit"
-          className="bg-yellow-600 font-[1rem] border-none outline rounded-[10px] cursor-pointer h-[2rem] w-[10rem] text-black mt-10 "
+          className="bg-yellow-600 font-[1rem] border-none  rounded-[10px] cursor-pointer h-[2rem] w-[10rem] text-black mt-10 "
         >
           Login
         </button>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/NavBar";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
