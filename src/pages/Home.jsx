a2dd29e3c695a989ddb83aeac81c5ddf13eae2e1
mucahitkarakus <mocococo.80@gmail.com>
2022-10-25 17:20:48 +0300

    create home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import Navbar from "../components/NavBar";
+import React from "react";
+
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
