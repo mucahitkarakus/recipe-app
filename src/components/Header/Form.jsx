5b5a43f5ecdd9b1edfc4bce323ed985226075257
mucahitkarakus <mocococo.80@gmail.com>
2022-10-27 12:27:35 +0300

    Form Element Done

---
+++ src/components/Header/Form.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="">
+        <input
+          className="bg-blue-400 border-none m-[20px] h-[40px] w-[200px] rounded-xl text-white outline-none  "
+          type="text"
+        />
+      </div>
+      <div>
+        <button className="text-white bg-blue-400 h-[40px] w-[100px] rounded-xl ">
+          Search
+        </button>
+      </div>
+      <div className="ml-5">
+        <select
+          name=""
+          id=""
+          className="w-[150px] h-[40px] text-white bg-blue-400 rounded-xl  "
+        >
+          <option value="">Breakfast</option>
+          <option value="">Brunch</option>
+          <option value="">Lunch</option>
+          <option value="">Dinner</option>
+          <option value="">TeaTime</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Form from "./Form";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="">
+      <h1 className="text-center font-semibold text-[50px] mt-5  ">Food App</h1>
+      <Form />
+    </div>
+  );
 };
 
 export default Header;
